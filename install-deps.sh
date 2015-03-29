b79d9c123de647625d9d31ad3557641411faf8cd
Bengt Martensson <barf@bengt-martensson.de>
2015-03-29 14:53:40 +0200

    Pom tuning and cleanup. Added install-deps.sh from Alec.

---
+++ install-deps.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#
+#  Install external dependencies using build-classpath from jpackage-utils
+#
+# Author: Alec Leamas
+#
+
+mvn install:install-file \
+    -DgroupId=com.hifiremote \
+    -DartifactId=DecodeIrCaller \
+    -Dversion=2.44 \
+    -Dpackaging=jar \
+    -Dfile=$( build-classpath com.hifiremote:DecodeIrCaller )
+
+mvn install:install-file \
+    -DgroupId=com.hifiremote \
+    -DartifactId=ExchangeIR \
+    -Dversion=0.0.8.2 \
+    -Dpackaging=jar \
+    -Dfile=$( build-classpath com.hifiremote:ExchangeIR )
+
+mvn install:install-file \
+    -DgroupId=org.rxtx \
+    -DartifactId=rxtx \
+    -Dversion=2.2 \
+    -Dpackaging=jar \
+    -Dfile=$( build-classpath org.rxtx:rxtx )
+
+mvn install:install-file \
+    -DgroupId=de.moldaner \
+    -DartifactId=wakeonlan \
+    -Dversion=1.0.0 \
+    -Dpackaging=jar \
+    -Dfile=$( build-classpath de.moldaner:wakeonlan )
+
+mvn install:install-file \
+    -DgroupId=com.mrallen \
+    -DartifactId=tonto \
+    -Dversion=1.44 \
+    -Dpackaging=jar \
+    -Dfile=$( build-classpath com.mrallen:tonto )
