332feedcdf5c6117c50355a04dcb5f735b32a035
Bengt Martensson <barf@bengt-martensson.de>
2015-04-13 20:24:48 +0200

    Updated build-decodeir.sh for checked-in autotools.

--- tools/build-decodeir.sh
+++ tools/build-decodeir.sh
@@ -2,12 +2,11 @@
 
 svn export -q svn://svn.code.sf.net/p/controlremote/code/trunk/decodeir
 cd decodeir
-sed  -i -e 's|<version>0.1</version>|<version>2.44</version>|' pom.xml
 mvn install
 
-# end here if compilating the shared library is not needed
-g++ -O2 -shared -fPIC \
-    -I/usr/lib/jvm/java/include -I/usr/lib/jvm/java/include/linux \
-    -o libDecodeIR.so DecodeIR.cpp
-sudo install /usr/local/lib64
+# end here if compilation of the shared library is not wanted
 
+autoreconf -fi
+./configure
+make
+sudo make install 
