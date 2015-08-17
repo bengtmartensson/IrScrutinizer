7a0c197d0e2ce409ea044510f15214a99af258f8
Bengt Martensson <barf@bengt-martensson.de>
2015-08-17 13:25:42 +0200

    For java-readline, now use branch autotools.

--- tools/build-java-readline.sh
+++ tools/build-java-readline.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 git clone https://github.com/bengtmartensson/java-readline
 cd java-readline
+git checkout autotools
 mvn install
 
 # end here if compilation of the shared library is not wanted
 
-make build-native
+# Need to have readline including its includes.
+# on Fedora, this is the packages readline AND readline-devel.
+# If not, configure will barf.
+
+./autogen.sh
+./configure
+make
 sudo make install
