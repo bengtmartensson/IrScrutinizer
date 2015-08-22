fda77d5b88d9bd8d788c4c6cd57301e9aea27490
Bengt Martensson <barf@bengt-martensson.de>
2015-08-22 20:20:15 +0200

    generate-pom.sh: do not assume being run from tools directory.

--- tools/generate-pom.sh
+++ tools/generate-pom.sh
@@ -3,4 +3,5 @@
 # Generate the pom in package.
 # It contains so many version numbers that are impossible to keep consistent otherwise.
 
-xsltproc --output ../pom.xml generate-pom.xsl ../pom-template.xml
+cd $(dirname $0)/..
+xsltproc --output pom.xml tools/generate-pom.xsl pom-template.xml
