f646293801b599f9ab779490b492cf50874de5a3
Bengt Martensson <barf@bengt-martensson.de>
2015-09-13 12:28:45 +0200

    New overloaded version of IrpUtils.parseLong.

--- IrpMaster/src/main/java/org/harctoolbox/IrpMaster/IrpUtils.java
+++ IrpMaster/src/main/java/org/harctoolbox/IrpMaster/IrpUtils.java
@@ -27,7 +27,7 @@ import java.util.TreeMap;
  * This class is a collection of useful utilities as static functions and constants.
  */
 public class IrpUtils {
-  
+
     public final static String defaultConfigfile = "data" + File.separator + "IrpProtocols.ini";
 
     public final static String dumbCharsetName = "US-ASCII";
@@ -51,7 +51,7 @@ public class IrpUtils {
     public final static int exitDynamicLinkError    = 9;
     public final static int exitThisCannotHappen    = 10;
     public final static int exitRestart             = 99; // An invoking script is supposed to restart the program
-    
+
     public final static String jp1WikiUrl = "http://www.hifi-remote.com/wiki/index.php?title=Main_Page";
     public final static String irpNotationUrl = "http://www.hifi-remote.com/wiki/index.php?title=IRP_Notation";
     public final static String decodeIrUrl = "http://www.hifi-remote.com/wiki/index.php?title=DecodeIR";
@@ -76,15 +76,15 @@ public class IrpUtils {
     public static String join(int beg, String[] s, String separator) {
         if (s == null || s.length <= beg)
             return "";
-        
+
         StringBuilder res = new StringBuilder();
         res.append(s[beg]);
         for (int i = beg+1; i < s.length; i++)
             res.append(separator).append(s[i]);
-        
+
         return res.toString();
     }
-    
+
     public static String join(String[] s, String separator) {
         return join(0, s, separator);
     }
@@ -92,42 +92,42 @@ public class IrpUtils {
     public static String join(String[] s, char separator) {
         return join(s, Character.toString(separator));
     }
-    
+
     public static String stringArray(int[] array) {
         if (array == null)
             return null;
         if (array.length == 0)
             return "[]";
-        
+
         StringBuilder result = new StringBuilder();
         result.append("[").append(array[0]);
         for (int i = 1; i < array.length; i++)
             result.append(", ").append(array[i]);
-        
+
         return result.append("]").toString();
     }
-    
+
     public static double l1Norm(Double[] sequence) {
         double sum = 0;
         for (Double d : sequence)
             sum += Math.abs(d);
         return sum;
     }
-    
+
     public static double l1Norm(double[] sequence) {
         double sum = 0;
         for (int i = 0; i < sequence.length; i++)
             sum += Math.abs(sequence[i]);
         return sum;
     }
-    
+
     public static double l1Norm(double[] sequence, int beg, int length) {
         double sum = 0;
         for (int i = beg; i < beg + length; i++)
             sum += Math.abs(sequence[i]);
         return sum;
     }
-    
+
     public static boolean isInstance(Object o, String classname) {
         try {
             return Class.forName("IrpMaster." + classname).isInstance(o);
@@ -136,17 +136,17 @@ public class IrpUtils {
         }
         return false;
     }
-    
+
     public static String spaces(int length) {
         byte[] buf = new byte[length];
         for (int i = 0; i < length; i++)
             buf[i] = 0x20;
         return new String(buf, IrpUtils.dumbCharset);
     }
-    
+
     /**
-     * Either opens a file (optionally for appending (if beginning with +)) or returns stdout. 
-     * 
+     * Either opens a file (optionally for appending (if beginning with +)) or returns stdout.
+     *
      * @param filename Either - for stdout, or a file name, or null. If starting with +, the file is opened in append mode, after removing the +-character.
      * @return Open PrintStream
      * @throws FileNotFoundException if FileOutputStream does
@@ -165,25 +165,25 @@ public class IrpUtils {
             return null;
         }
     }
-    
+
     /**
-     * Either opens an input file or returns stdin. 
-     * 
+     * Either opens an input file or returns stdin.
+     *
      * @param filename
      * @return Open InputStream
-     * @throws FileNotFoundException 
+     * @throws FileNotFoundException
      */
     public static InputStream getInputSteam(String filename) throws FileNotFoundException {
         return filename.equals("-") ? System.in : new FileInputStream(filename);
     }
-    
-    /** 
+
+    /**
      * The power function for long arguments.
-     * 
+     *
      * @param x long
      * @param y long, non-negative
-     * @return x raised to the y'th power 
-     * 
+     * @return x raised to the y'th power
+     *
      * @throws ArithmeticException
      */
     public static long power(long x, long y) {
@@ -194,22 +194,22 @@ public class IrpUtils {
             r *= x;
         return r;
     }
-    
+
     /**
      * Reverses the bits, living in a width-bit wide world.
-     * 
+     *
      * @param x
      * @param width
-     * @return bitreversed 
+     * @return bitreversed
      */
-    
+
     public static long reverse(long x, int width) {
         long y = Long.reverse(x);
         if (width > 0)
             y >>>= Long.SIZE - width;
         return y;
     }
-    
+
     /**
      * Parses integers of base 2 (prefix "0b"  or "%", 8 (leading 0), 10, or 16 (prefix "0x).
      * If argument special is true, allows intervals 123..456 or 123:456 by ignoring upper part.
@@ -234,23 +234,33 @@ public class IrpUtils {
                s.startsWith("0") ? Long.parseLong(s.substring(1), 8) :
                Long.parseLong(s);
     }
-    
+
+    /**
+     * Parses integers of base 2 (prefix "0b"  or "%", 8 (leading 0), 10, or 16 (prefix "0x).
+     *
+     * @param str String to be parsed
+     * @return long integer.
+     */
+    public static long parseLong(String str) {
+        return parseLong(str,false);
+    }
+
     public static long parseUpper(String str) {
         String[] s = str.split("\\.\\.");
         if (s.length == 1)
             s = str.split(":");
-        
+
         return (s.length == 2) ? parseLong(s[1], false) : invalid;
     }
-    
+
     public static long variableGet(Map<String, Long> map, String name) {
         return map.containsKey(name) ? map.get(name) :  invalid;
     }
-    
+
     /**
      * Produces a header in the spirit of Makehex. Follows the convention of variable ordering:
      * D, (S), F, (T), then the rest alphabetically ordered,
-     * 
+     *
      * @param params HashMap<String, Long> of input parameters.
      * @return Nicely formatted header (String)
      */
@@ -260,30 +270,30 @@ public class IrpUtils {
         map.remove("F");
         map.remove("S");
         map.remove("T");
-        
+
         String result = formatVariable(params, "D", "Device Code: ", "")
                 + formatVariable(params, "S", ".", "")
                 + " "
                 + formatVariable(params, "F", "Function: ", "")
                 + formatVariable(params, "T", ", Toggle: ", "");
-        
+
         for (String var : map.keySet())
             result += formatVariable(params, var, ", " + var + "=", "");
-        
+
         return result;
     }
-    
+
     private static String formatVariable(HashMap<String, Long>map, String name, String prefix, String postfix) {
         if (!map.containsKey(name))
             return "";
-        
+
         return prefix + map.get(name) + postfix;
     }
 
     public static void main(String[] args) {
         for (String arg : args)
             System.out.println(arg);
-        
+
         int arg_i = 0;
         String outFile = args[arg_i++];
         String configFilename = args[arg_i++];
@@ -291,7 +301,7 @@ public class IrpUtils {
         String protocolName = args[arg_i++];
         String device = args[arg_i++];
         String OBC = args[arg_i++];
-        
+
         int status = IrpMaster.makeHex(outFile, true, configFilename, preamble, protocolName, device, OBC);
         System.out.println(status);
     }
