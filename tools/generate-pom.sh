eb195c55d91c2d1e8ae6b5976e557dfccac81bf6
Bengt Martensson <barf@bengt-martensson.de>
2016-04-26 20:28:59 +0200

    Bumped the version numbers to 1.2, except IrScrutinizer (1.2beta).

--- tools/generate-pom.sh
+++ tools/generate-pom.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Generate the pom in package.
-# It contains so many version numbers that are impossible to keep consistent otherwise.
+# Generate the pom in package. It picks up the version numbers in <subpackage>/pom.xml
+# and puts them in the main pom.xml.
 
 cd $(dirname $0)/..
 xsltproc --output pom.xml tools/generate-pom.xsl pom-template.xml
