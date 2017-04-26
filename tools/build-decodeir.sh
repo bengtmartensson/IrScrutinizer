543b40b6014366d2a2bc7d91850b09a156b4e78c
Bengt Martensson <barf@bengt-martensson.de>
2017-04-26 17:32:14 +0200

    decodeir: make sure that correct revison is checked out (just to be sure...)

--- tools/build-decodeir.sh
+++ tools/build-decodeir.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-svn export -q svn://svn.code.sf.net/p/controlremote/code/trunk/decodeir
-cd decodeir
+PROGNAME=decodeir
+svn export -r 1358 -q svn://svn.code.sf.net/p/controlremote/code/trunk/${PROGNAME}
+cd ${PROGNAME}
 mvn install
 
 # end here if compilation of the shared library is not wanted
