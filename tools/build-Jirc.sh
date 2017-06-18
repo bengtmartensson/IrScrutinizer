112de59c281fe431e7d165f374f4ea7c8f6e7c6c
Bengt Martensson <barf@bengt-martensson.de>
2017-06-18 21:12:13 +0200

    Changes for Travis, #215.

--- tools/build-Jirc.sh
+++ tools/build-Jirc.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-git clone https://github.com/bengtmartensson/DevSlashLirc.git
-cd DevSlashLirc
+git clone https://github.com/bengtmartensson/Jirc.git
+cd Jirc
 
-# Fake media/lirc.h, normally in /usr/local/include/lirc/include/media/lirc.h
-#mkdir -p target/generated-sources/c++/org/harctoolbox/devslashlirc/media
-#wget -O  target/generated-sources/c++/org/harctoolbox/devslashlirc/media/lirc.h https://sourceforge.net/p/lirc/git/ci/master/tree/include/media/lirc.h?format=raw
-
-#make lib
 mvn install -Dmaven.test.skip=true
