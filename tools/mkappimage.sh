6b2f8ece227cbcb9c89256d4d1a5f119887bf78c
probonopd <probonopd@users.noreply.github.com>
2019-06-13 19:42:10 +0000

    Bundle subset of Java and switch to appimagetool

--- tools/mkappimage.sh
+++ tools/mkappimage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Create a directory for AppImage, and call AppImageAssistant.
+# Create a directory for AppImage, and call appimagetool.
 # Should be called from the IrScrutinizer directory.
 #
 # TODO: should check membership in groups dialout, lock, and lirc,
@@ -15,13 +15,12 @@ VERSION=$2
 APPIMAGEKITDIR=$3
 MYPROG_LOWER=$(echo ${APPNAME} | tr A-Z a-z)
 
-APPIMAGEDIR=target/AppImage
-MYPROG_HOME=${APPIMAGEDIR}/usr/share/${MYPROG_LOWER}
-USR_BIN=${APPIMAGEDIR}/usr/bin
+APPDIR=target/AppDir
+MYPROG_HOME=${APPDIR}/usr/share/${MYPROG_LOWER}
+USR_BIN=${APPDIR}/usr/bin
 WRAPPER=${USR_BIN}/${MYPROG_LOWER}
-APPIMAGE=target/${APPNAME}-${VERSION}-x86_64.AppImage
 
-rm -rf ${APPIMAGEDIR}
+rm -rf ${APPDIR}
 
 # Copy stuff to MYPROG_HOME
 install -d ${MYPROG_HOME}
@@ -48,12 +47,12 @@ install --mode=666 target/contributed/import/*.sh ${MYPROG_HOME}/contributed/imp
 install --mode=444 target/contributed/import/*.xsl ${MYPROG_HOME}/contributed/import
 
 # Top level
-install --mode=555 ${APPIMAGEKITDIR}/AppRun ${APPIMAGEDIR}
-install --mode=444 target/${APPNAME}.png ${APPIMAGEDIR}/${MYPROG_LOWER}.png
+install --mode=555 ${APPIMAGEKITDIR}/AppRun ${APPDIR}
+install --mode=444 target/${APPNAME}.png ${APPDIR}/${MYPROG_LOWER}.png
 sed -e "s|Exec=.*|Exec=${MYPROG_LOWER}.wrapper|" \
     -e "s|Icon=.*|Icon=${MYPROG_LOWER}|" \
-    -e "s|Name=.*|Name=${APPNAME}|" < target/${MYPROG_LOWER}.desktop > ${APPIMAGEDIR}/${MYPROG_LOWER}.desktop
-chmod 444 ${APPIMAGEDIR}/${MYPROG_LOWER}.desktop
+    -e "s|Name=.*|Name=${APPNAME}|" < target/${MYPROG_LOWER}.desktop > ${APPDIR}/${MYPROG_LOWER}.desktop
+chmod 444 ${APPDIR}/${MYPROG_LOWER}.desktop
 
 # Fill "usr/bin"
 install -d ${USR_BIN}
@@ -61,10 +60,17 @@ echo "#!/bin/sh"                                                                
 echo "unset XDG_DATA_DIRS"                                                          >> ${WRAPPER}
 echo "IRSCRUTINIZERHOME=\${APPDIR}/usr/share/${MYPROG_LOWER}"                       >> ${WRAPPER}
 echo 'cd ${IRSCRUTINIZERHOME}'                                                      >> ${WRAPPER}
-echo "java -Djava.library.path=${JAVA_LIBRARY_PATH} -jar \${IRSCRUTINIZERHOME}/${APPNAME}-jar-with-dependencies.jar \"\$@\"" >> ${WRAPPER}
+echo "java -Djava.library.path=${JAVA_LIBRARY_PATH} -Xquickstart -jar \${IRSCRUTINIZERHOME}/${APPNAME}-jar-with-dependencies.jar \"\$@\"" >> ${WRAPPER}
 chmod 555 ${WRAPPER}
 install --mode=555 ${APPIMAGEKITDIR}/desktopintegration ${USR_BIN}/${MYPROG_LOWER}.wrapper
 
+# Bundle subset of Java
+wget -c https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.3%2B7_openj9-0.14.3/OpenJDK11U-jdk_x64_linux_openj9_11.0.3_7_openj9-0.14.3.tar.gz
+tar xf OpenJDK11U-jdk_x64_linux_openj9_11.0.3_7_openj9-0.14.3.tar.gz
+./jdk-11.0.3+7/bin/jlink' --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules java.base,java.datatransfer,java.desktop,java.logging,java.xml --output usr
+cp -Rf usr ${APPDIR}/
+
 # Invoke the builder
-rm -f ${APPIMAGE}
-${APPIMAGEKITDIR}/AppImageAssistant ${APPIMAGEDIR} ${APPIMAGE}
+wget -c "https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage"
+chmod +x ./appimagetool-x86_64.AppImage
+./appimagetool-x86_64.AppImage ${APPDIR}
