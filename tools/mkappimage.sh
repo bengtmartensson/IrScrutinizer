99014d22fecae0f257676039ff5ec9936487b06f
Bengt Martensson <barf@bengt-martensson.de>
2019-06-20 13:46:07 +0200

    More tweaks for the AppImage. Cf. #279.

--- tools/mkappimage.sh
+++ tools/mkappimage.sh
@@ -2,12 +2,9 @@
 #
 # Create a directory for AppImage, and call appimagetool.
 # Should be called from the IrScrutinizer top directory.
-#
-# TODO: should check membership in groups dialout, lock, and lirc,
-# as in irscrutinizer.sh
 
 if [ "`uname -m`" != "x86_64" ] ; then
-    echo "Presently, AppImages can onby be built on x86_64, quitting."
+    echo "Presently, AppImages can only be built on x86_64, quitting."
     exit 0
 fi
 
@@ -23,7 +20,6 @@ USR=${APPDIR}/usr
 USR_BIN=${USR}/bin
 WRAPPER=${USR_BIN}/${MYPROG_LOWER}
 APPRUN=tools/AppRun
-DESKTOPINTEGRATION=tools/desktopintegration
 JAVA_MODULES=java.base,java.datatransfer,java.desktop,java.logging,java.xml
 
 rm -rf ${APPDIR}
@@ -35,7 +31,7 @@ if [ "x${JAVA_TAR_GZ}y" != "xy" -a -f "${JAVA_TAR_GZ}" ] ; then
     JAVA_ROOT=`echo jdk*`
     ${JAVA_ROOT}/bin/jlink --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules ${JAVA_MODULES} --output ${USR}
     rm -rf ${JAVA_ROOT}
-    JAVA_PATH=\${APPDIR}/usr/bin/
+    JAVA_PATH=\${APP_ROOT}/usr/bin/
     JAVA_QUICKSTART=-Xquickstart
 fi
 
@@ -62,31 +58,58 @@ install --mode=444 target/contributed/import/*.xsl ${MYPROG_HOME}/contributed/im
 # Top level
 install --mode=555 ${APPRUN} ${APPDIR}
 install --mode=444 target/${APPNAME}.png ${APPDIR}/${MYPROG_LOWER}.png
-sed -e "s|Exec=.*|Exec=${MYPROG_LOWER}.wrapper|" \
+sed -e "s|Exec=.*|Exec=${MYPROG_LOWER}|" \
     -e "s|Icon=.*|Icon=${MYPROG_LOWER}|" \
     -e "s|Name=.*|Name=${APPNAME}|" < target/${MYPROG_LOWER}.desktop > ${APPDIR}/${MYPROG_LOWER}.desktop
 chmod 444 ${APPDIR}/${MYPROG_LOWER}.desktop
-install -d ${USR}/share/metainfo
+#install -d ${USR}/share/metainfo
 #install --mode=444 src/main/resources/appdata.xml ${USR}/share/metainfo/${MYPROG_LOWER}.appdata.xml
 
 # Fill "usr/bin"
 install -d ${USR_BIN}
 
+# Create wrapper
 cat > ${WRAPPER} <<EOF
 #!/bin/sh
 
 unset XDG_DATA_DIRS
-IRSCRUTINIZERHOME=\${APPDIR}/usr/share/${MYPROG_LOWER}
+IRSCRUTINIZERHOME=\${APP_ROOT}/usr/share/${MYPROG_LOWER}
+
+checkgroup()
+{
+    if grep \$1 /etc/group > /dev/null ; then
+	if ! groups | grep \$1 > /dev/null ; then
+            MESSAGE=\${MESSAGE}\$1,
+	fi
+    fi
+}
+
+# Check that the use is a member of some groups ...
+checkgroup dialout
+checkgroup lock
+checkgroup lirc
+
+# ... if not, barf, and potentially bail out.
+if [ "x\${MESSAGE}" != "x" ] ; then
+     # Remove last , of $MESSAGE
+    MESSAGE=\$(echo \${MESSAGE} | sed -e "s/,\$//")
+    MESSAGEPRE="You are not a member of the group(s) "
+    MESSAGETAIL=", so you will probably not have access to some devices.\nYou probably want to correct this. Otherwise, functionality will be limited.\n\nDepending on your operating system, the command for fixing this may be \"sudo usermod -aG \${MESSAGE} \${USER}\".\n\nProceed anyhow?"
+    if ! "${JAVA_PATH}java" -classpath "\${IRSCRUTINIZERHOME}/IrScrutinizer-jar-with-dependencies.jar" \
+           org.harctoolbox.guicomponents.StandalonePopupAnnoyer "\${MESSAGEPRE}\${MESSAGE}\${MESSAGETAIL}" "\$@" ; then
+        exit 1
+    fi
+fi
+
 cd \${IRSCRUTINIZERHOME}
 
 # Since the lock directory is sometimes different from /var/lock,
 # we prefer the system's rxtx (if existing) over our own,
-# which is therefore put last in the path.
-${JAVA_PATH}java -Djava.library.path=/usr/lib64/rxtx:/usr/lib64:/usr/lib/rxtx:/usr/lib:\${IRSCRUTINIZERHOME}/Linux-amd64 ${JAVA_QUICKSTART} -jar \${IRSCRUTINIZERHOME}/${APPNAME}-jar-with-dependencies.jar "\$@"
+# which is why we put our own last in the path.
+"${JAVA_PATH}java" -Djava.library.path=/usr/lib64/rxtx:/usr/lib64:/usr/lib/rxtx:/usr/lib:\${IRSCRUTINIZERHOME}/Linux-amd64 ${JAVA_QUICKSTART} -jar \${IRSCRUTINIZERHOME}/${APPNAME}-jar-with-dependencies.jar "\$@"
 EOF
 
 chmod 555 ${WRAPPER}
-install --mode=555 ${DESKTOPINTEGRATION} ${USR_BIN}/${MYPROG_LOWER}.wrapper
 
 # Invoke the builder
 #wget -c "https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage"
