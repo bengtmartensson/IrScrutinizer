035efd0d318a6e7513ccae763377020a76a4d791
probonopd <probonopd@users.noreply.github.com>
2019-06-20 23:26:19 +0000

    Update build-appimagekit.sh

--- tools/build-appimagekit.sh
+++ tools/build-appimagekit.sh
@@ -5,12 +5,5 @@
 OUTPUTDIR=appimagekit
 
 mkdir ${OUTPUTDIR}
-wget -O ${OUTPUTDIR}/AppRun "https://github.com/probonopd/AppImageKit/releases/download/5/AppRun" # (64-bit)
+wget -O ${OUTPUTDIR}/AppRun "https://github.com/AppImage/AppImageKit/releases/download/12/AppRun-x86_64" # (64-bit)
 chmod +x ${OUTPUTDIR}/AppRun
-
-wget -O ${OUTPUTDIR}/AppImageAssistant "https://github.com/probonopd/AppImageKit/releases/download/5/AppImageAssistant" # (64-bit)
-chmod +x ${OUTPUTDIR}/AppImageAssistant
-
-wget https://github.com/probonopd/AppImageKit/archive/5.tar.gz
-tar -x -f 5.tar.gz --strip-components=1 -C ${OUTPUTDIR} AppImageKit-5/desktopintegration
-rm 5.tar.gz
