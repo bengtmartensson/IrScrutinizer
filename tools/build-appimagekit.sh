cfac9942c57ac2cb8b2f8af556d71200f71fb053
Bengt Martensson <barf@bengt-martensson.de>
2019-06-21 15:33:06 +0200

    Nuked obsolete file.

--- tools/build-appimagekit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# The name of this file is misleading ;-).
-
-OUTPUTDIR=appimagekit
-
-mkdir ${OUTPUTDIR}
-wget -O ${OUTPUTDIR}/AppRun "https://github.com/AppImage/AppImageKit/releases/download/12/AppRun-x86_64" # (64-bit)
-chmod +x ${OUTPUTDIR}/AppRun
