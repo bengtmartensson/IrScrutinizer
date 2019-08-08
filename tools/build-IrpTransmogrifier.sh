d0a3383974a0ba3f9bbdb2267f7c4c4b34cfa54b
Bengt Martensson <barf@bengt-martensson.de>
2019-08-08 21:23:17 +0200

    travis: build IrpTransmogrifier 1-0.0 too (for now).

--- tools/build-IrpTransmogrifier.sh
+++ tools/build-IrpTransmogrifier.sh
@@ -3,3 +3,6 @@ git clone https://github.com/bengtmartensson/IrpTransmogrifier.git
 cd IrpTransmogrifier
 
 mvn install -Dmaven.test.skip=true
+
+git checkout Version-1.0.0
+mvn install -Dmaven.test.skip=true
