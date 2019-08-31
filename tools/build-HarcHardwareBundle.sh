41b0df6268f8f5067442e9a5e3fd0a143b34d1b6
Bengt Martensson <barf@bengt-martensson.de>
2019-08-31 14:40:10 +0200

    Set version to 2.1.0, using IrpTransmogrifier 1.1.0, Girr 2.1.0, HarcHardwareBundle 2.0.1.

--- tools/build-HarcHardwareBundle.sh
+++ tools/build-HarcHardwareBundle.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 git clone https://github.com/bengtmartensson/HarcHardwareBundle.git
 cd HarcHardwareBundle
-#git checkout IrpMaster
 
+git checkout Version-2.0.1
 mvn install -Dmaven.test.skip=true
