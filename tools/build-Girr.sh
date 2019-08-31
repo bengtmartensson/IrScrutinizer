5e63952a8e46939daf1e7d6f5ddc792f09122b0a
Bengt Martensson <barf@bengt-martensson.de>
2019-08-31 14:55:37 +0200

    Build Girr Version 2.0.1 too.

--- tools/build-Girr.sh
+++ tools/build-Girr.sh
@@ -3,5 +3,5 @@ git clone https://github.com/bengtmartensson/Girr.git
 cd Girr
 mvn install -Dmaven.test.skip=true
 
-#git checkout Version-2.0.1
-#mvn install -Dmaven.test.skip=true
+git checkout Version-2.0.1
+mvn install -Dmaven.test.skip=true
