fab715e311d3c3cd281c8ac42922e549a0a47c70
Bengt Martensson <barf@bengt-martensson.de>
2019-12-05 09:12:42 +0100

    travis: use Maven Central Repository versions of our dependencies.

--- tools/build-HarcHardwareBundle.sh
+++ tools/build-HarcHardwareBundle.sh
@@ -2,5 +2,5 @@
 git clone https://github.com/bengtmartensson/HarcHardwareBundle.git
 cd HarcHardwareBundle
 
-git checkout Version-2.0.1
+#git checkout Version-2.0.1
 mvn install -Dmaven.test.skip=true
