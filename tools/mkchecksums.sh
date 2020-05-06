107f3907d86024f1fc0ae43c8ab7a9dff9554964
Bengt Martensson <barf@bengt-martensson.de>
2020-05-06 13:37:56 +0200

    Rename checksum -> checksums. Dont upload MacOS compressed app.
    (is replaced by the dmg file).

--- tools/mkchecksums.sh
+++ tools/mkchecksums.sh
@@ -5,11 +5,11 @@ ALGORITHMS="md5 sha1 sha512"
 for algo in ${ALGORITHMS} ; do
     #test `${algo}sum -v /dev/null` || continue
     echo "Computing $algo checksums..." >&2
-    rm -f checksum.${algo}
+    rm -f checksums.${algo}
     # Allow for having non-existing arguments without confusing error messages
     for f in "$@" ; do
         if [ -f $f ] ; then
-            ${algo}sum "$f" >> checksum.${algo}
+            ${algo}sum "$f" >> checksums.${algo}
         fi
     done
 done
