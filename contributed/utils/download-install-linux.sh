6da91597e6d04ef5f98827f288fbd506fdd95780
Bengt Martensson <barf@bengt-martensson.de>
2021-03-13 16:00:31 +0100

    Minor cleanups and fixes.

--- contributed/utils/download-install-linux.sh
+++ contributed/utils/download-install-linux.sh
@@ -5,6 +5,9 @@
 # Author: Bengt Martensson
 # License: public domain
 
+PROJECT=IrScrutinizer
+PROJECT_LOWERCASE=$(echo ${PROJECT} | tr '[A-Z]' '[a-z]')
+
 if [ $(id -u) -eq 0 ] ; then
     PREFIX=/usr/local
 else
@@ -13,7 +16,7 @@ fi
 
 # Where the files are installed, modify if desired.
 # Can be overridden from the command line.
-APPHOME=${PREFIX}/share/irscrutinizer
+APPHOME=${PREFIX}/share/${PROJECT_LOWERCASE}
 
 # Where the executable links go-
 # Can be overridden from the command line.
@@ -37,9 +40,9 @@ fi
 #SCALE_FACTOR=1
 
 # Should probably not change
-URL_VERSION=http://harctoolbox.org/downloads/IrScrutinizer.version
+URL_VERSION=http://harctoolbox.org/downloads/${PROJECT}.version
 
-TMP_DIR=/tmp/downloader$$
+TMP_DIR=${TMPDIR:-/tmp}/downloader$$
 
 usage()
 {
@@ -53,8 +56,9 @@ usage()
     echo "    -?, --help                        Display this help and exit."
 #    echo "    -j, --java command-for-java       Command to invoke Java, default \"${JAVA}\"."
 #    echo "    -s, --scale scale-factor          scale factor for the GUI, default ${SCALE_FACTOR}. Not supported by all JVMs."
-    echo "    -h, --home install-dir       Directory in which to install, default ${APPHOME}."
+    echo "    -h, --home install-dir            Directory in which to install, default ${APPHOME}."
     echo "    -l, --link directory-for-links    Directory in which to create start links, default ${LINKDIR}."
+    echo "    -u, --uninstall                   Uninstall previous installation."
     echo ""
     echo "This script should be run with the privileges necessary for writing"
     echo "to the locations selected."
@@ -77,22 +81,42 @@ while [ -n "$1" ] ; do
         -h | --home | --rmhome ) shift
                                 APPHOME="$1"
                                 ;;
+        -u | --uninstall )      UNINSTALL="y"
+                                ;;
         * )                     ZIP="$1"
                                 ;;
     esac
     shift
 done
 
+if [ -n "${UNINSTALL}" ] ; then
+    read -p "You sure you what to deinstall IrScrutinizer in directory ${APPHOME} (y/n)? " ans
+    if [ "${ans}" != "y" ] ; then
+        echo "Bailing out, nothing deleted."
+        exit 0
+    fi
+
+    rm -rf "${APPHOME}"
+    rm -f ${DESKTOPDIR}/${PROJECT}.desktop
+    rm -f ${LINKDIR}/${PROJECT_LOWERCASE}
+    rm -f ${LINKDIR}/irptransmogrifier
+
+    echo "${PROJECT} has been successfully uninstalled."
+    echo "Personal configuration files have not been deleted."
+    exit 0
+fi
+
 if [ -z ${ZIP} ] ; then
     if [ ! -d ${TMP_DIR} ] ; then
         mkdir -p ${TMP_DIR} || exit 1
     fi
     cd ${TMP_DIR} || exit 1
-    wget ${URL_VERSION}
+    wget --no-verbose ${URL_VERSION}
     VERSION=$(sed -e 's/IrScrutinizer version //' IrScrutinizer.version)
-    FILE=IrScrutinizer-${VERSION}-bin.zip
-    wget https://github.com/bengtmartensson/IrScrutinizer/releases/download/Version-${VERSION}/${FILE}
+    FILE=${PROJECT}-${VERSION}-bin.zip
+    wget --no-verbose https://github.com/bengtmartensson/IrScrutinizer/releases/download/Version-${VERSION}/${FILE}
     ZIP=${TMP_DIR}/${FILE}
+    DID_DOWNLOAD="y"
 fi
 
 if [ ! -d "${APPHOME}" ] ; then
@@ -103,3 +127,14 @@ cd "${APPHOME}" || exit 1
 rm -rf * || exit 1
 unzip -q "${ZIP}" || exit 1
 sh ./setup-irscrutinizer.sh || exit 1
+
+if [ -n "${DID_DOWNLOAD}" ] ; then
+    if tty --quiet ; then
+       read -p "Delete downloaded files in ${TMP_DIR} (y/n)? " ans
+        if [ "${ans}" = "y" ] ; then
+            rm -r ${TMP_DIR}
+        fi
+    else
+        rm -r ${TMP_DIR}
+    fi
+fi
