1ad18643e3435546db18ab492ead2b56c13f41d4
Bengt Martensson <barf@bengt-martensson.de>
2025-04-25 18:44:06 +0200

    Add forgotten test file.
    Known problem: #552.

---
+++ src/test/java/org/harctoolbox/irscrutinizer/GuiMainNGTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2024 Bengt Martensson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.harctoolbox.irscrutinizer;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author bengt
+ */
+@SuppressWarnings("UseOfSystemOutOrSystemErr")
+public class GuiMainNGTest {
+
+    private static final String NEC1_PRONTO =      "0000 006C 0022 0002 015B 00AD 0016 0016 0016 0016 0016 0041 0016 0041 0016 0016 0016 0016 0016 0016 0016 0016 0016 0016 0016 0041 0016 0016 0016 0016 0016 0016 0016 0041 0016 0016 0016 0016 0016 0016 0016 0016 0016 0016 0016 0041 0016 0041 0016 0041 0016 0016 0016 0016 0016 0041 0016 0041 0016 0041 0016 0016 0016 0016 0016 0016 0016 0041 0016 0041 0016 06A4 015B 0057 0016 0E6C";
+    private static final String NEC1_AS_SEQUENCE = "0000 006C 0024 0000 015B 00AD 0016 0016 0016 0016 0016 0041 0016 0041 0016 0016 0016 0016 0016 0016 0016 0016 0016 0016 0016 0041 0016 0016 0016 0016 0016 0016 0016 0041 0016 0016 0016 0016 0016 0016 0016 0016 0016 0016 0016 0041 0016 0041 0016 0041 0016 0016 0016 0016 0016 0041 0016 0041 0016 0041 0016 0016 0016 0016 0016 0016 0016 0041 0016 0041 0016 06A4 015B 0057 0016 0E6C";
+    private static final String NEC2_PRONTO =      "0000 006C 0000 0022 015B 00AD 0016 0016 0016 0016 0016 0041 0016 0041 0016 0016 0016 0016 0016 0016 0016 0016 0016 0016 0016 0041 0016 0016 0016 0016 0016 0016 0016 0041 0016 0016 0016 0016 0016 0016 0016 0016 0016 0016 0016 0041 0016 0041 0016 0041 0016 0016 0016 0016 0016 0041 0016 0041 0016 0041 0016 0016 0016 0016 0016 0016 0016 0041 0016 0041 0016 06A4";
+    private static final String NEC_AS_SEQUENCE =  "0000 006C 0022 0000 015B 00AD 0016 0016 0016 0016 0016 0041 0016 0041 0016 0016 0016 0016 0016 0016 0016 0016 0016 0016 0016 0041 0016 0016 0016 0016 0016 0016 0016 0041 0016 0016 0016 0016 0016 0016 0016 0016 0016 0016 0016 0041 0016 0041 0016 0041 0016 0016 0016 0016 0016 0041 0016 0041 0016 0041 0016 0016 0016 0016 0016 0016 0016 0041 0016 0041 0016 06A4";
+
+    public GuiMainNGTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+
+    /**
+     * Test of scrutinizeSignal method, of class GuiMain.
+     */
+    @Test
+
+    public void testScrutinizeSignal() throws Exception {
+        System.out.println("scrutinizeSignal");
+        GuiMain instance = new GuiMain();
+        String decode = instance.scrutinizeSignal(NEC1_PRONTO);
+        assertEquals(decode, "NEC1: {D=12,S=34,F=56}");
+        decode = instance.scrutinizeSignal(NEC1_AS_SEQUENCE);
+        assertEquals(decode, "NEC1: {D=12,S=34,F=56}, beg=0, end=71, reps=1");
+        decode = instance.scrutinizeSignal(NEC2_PRONTO);
+        assertEquals(decode, "NEC2: {D=12,S=34,F=56}");
+        decode = instance.scrutinizeSignal(NEC_AS_SEQUENCE);
+        assertEquals(decode, "NEC: {D=12,S=34,F=56}, beg=0, end=67");
+    }
+}
