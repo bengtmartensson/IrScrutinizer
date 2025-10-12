844876ac6ba937706f56ddaacd91685d943e2d8b
Bengt Martensson <barf@bengt-martensson.de>
2025-10-12 15:44:32 +0200

    Remote dead code.

--- src/main/java/org/harctoolbox/irscrutinizer/importer/ControlTowerIrDatabase.java
+++ src/main/java/org/harctoolbox/irscrutinizer/importer/ControlTowerIrDatabase.java
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2016 Bengt Martensson.
+Copyright (C) 2016, 2025 Bengt Martensson.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -20,10 +20,8 @@ package org.harctoolbox.irscrutinizer.importer;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -38,7 +36,6 @@ import java.util.logging.Logger;
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
-import javax.json.JsonObjectBuilder;
 import javax.json.JsonValue;
 import javax.json.stream.JsonParser;
 import org.harctoolbox.girr.Command;
@@ -57,11 +54,10 @@ public class ControlTowerIrDatabase extends DatabaseImporter implements IRemoteS
 
     // See https://www.globalcache.com/files/docs/API-GlobalIRDB_ver1.pdf page 6f.
     private static String httpEncode(String s) throws UnsupportedEncodingException {
-        String str = s.replaceAll("&", "xampx").replaceAll("/", "xfslx")
+        return s.replaceAll("&", "xampx").replaceAll("/", "xfslx")
                 .replaceAll(">", "xgtx").replaceAll("<", "xltx")
                 .replaceAll(":", "xcolx").replaceAll("\\?", "xquex")
                 .replaceAll("%", "xmodx").replaceAll("\\+", "xaddx");
-        return str;
     }
 
     @SuppressWarnings("UseOfSystemOutOrSystemErr")
@@ -69,68 +65,28 @@ public class ControlTowerIrDatabase extends DatabaseImporter implements IRemoteS
         Props props = new Props(null);
         Importer.setProperties(props);
         try {
-            ControlTowerIrDatabase gcdb = new ControlTowerIrDatabase(args[0], args[1], true);
-            //System.out.println(gcdb.getTypes());
-            //System.out.println(gcdb.getManufacturers());
+            ControlTowerIrDatabase gcdb = new ControlTowerIrDatabase(true);
             System.out.println(gcdb.getDeviceTypes("Sony"));
             System.out.println(gcdb.getModels("Sony", "Projector"));
             System.out.println(gcdb.getManufacturers("Blu Ray"));
             System.out.println(gcdb.getModels("Oppo Digital", "Blu Ray"));
             System.out.println(gcdb.getModel(1758));
-            gcdb.login();
-            System.out.println(gcdb.getStatus());
-            gcdb.getCodeset(1758, false);
-            System.out.println(gcdb.getStatus());
-            gcdb.logout();
         } catch (MalformedURLException ex) {
             Logger.getLogger(ControlTowerIrDatabase.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (LoginException ex) {
-            System.err.println("Login failed: " + ex.getMessage());
         } catch (IOException | URISyntaxException ex) {
             System.err.println(ex.getMessage());
         }
     }
 
     private boolean verbose = false;
-    private String apiKey;
-    private final String email;
-    private final String password;
-    private int codesRequestedToday;
-    private String accountType;
-    private String name;
-    private String company;
-
     private Map<String, String> manufacturerMap;
     private Map<String, String> typesMap;
     private String manufacturer;
     private String deviceType;
     private RemoteSet remoteSet;
 
-    public ControlTowerIrDatabase(String apiKey, boolean verbose) {
-        super(globalCacheDbOrigin);
-        this.company = null;
-        this.name = null;
-        this.accountType = null;
-        this.codesRequestedToday = -1;
-        this.email = null;
-        this.password = null;
-        this.apiKey = apiKey;
-        this.verbose = verbose;
-    }
-
     public ControlTowerIrDatabase(boolean verbose) {
-        this(null, null, verbose);
-    }
-
-    public ControlTowerIrDatabase(String email, String password, boolean verbose) {
         super(globalCacheDbOrigin);
-        this.company = null;
-        this.name = null;
-        this.accountType = null;
-        this.codesRequestedToday = -1;
-        this.email = email;
-        this.password = password;
-        this.apiKey = null;
         this.verbose = verbose;
     }
 
@@ -150,97 +106,13 @@ public class ControlTowerIrDatabase extends DatabaseImporter implements IRemoteS
 
     private JsonValue readFrom(Reader reader) throws IOException {
         JsonParser parser = Json.createParser(reader);
-        JsonParser.Event x = parser.next();
+        parser.next();
         return parser.getValue();
     }
-
-    @SuppressWarnings("UseOfSystemOutOrSystemErr")
-    private JsonObject postAndGetObject(String str, String payload) throws MalformedURLException, IOException, LoginException, URISyntaxException {
-        URL url = new URI(protocol, null, controlTowerIrDatabaseHost, portNo, path + str, null, null).toURL();
-        if (verbose)
-            System.err.println("Opening (POST) " + url.toString());
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("Accept", "application/json");
-        //if (payload != null) {
-            connection.setRequestProperty("Content-Type", "application/json; charset=US-ASCII");
-            connection.setRequestProperty("Content-Length", "" + Integer.toString(payload == null ? 0 : payload.getBytes(Charset.forName("US-ASCII")).length));
-            connection.setRequestProperty("Content-Language", "en-US");
-        //}
-
-        connection.setUseCaches(false);
-        connection.setDoInput(true);
-        connection.setDoOutput(payload != null);
-        if (payload != null) {
-            try (OutputStream outputStream = connection.getOutputStream()) {
-                outputStream.write(payload.getBytes("US-ASCII"));
-                outputStream.flush();
-            }
-        }
-
-        int httpResult = connection.getResponseCode();
-
-        if (httpResult != HttpURLConnection.HTTP_OK) {
-            connection.disconnect();
-            throw new LoginException("HTTP error " + httpResult);
-        }
-
-        InputStream is = connection.getInputStream();
-        InputStreamReader isr = new InputStreamReader(is, Charset.forName("US-ASCII"));
-        JsonObject response = readFrom(isr).asJsonObject();
-        connection.disconnect();
-        return response;
-    }
-
-    private String apiKeyString(char separator) {
-        return apiKey == null ? "" : (separator + "apiKey=" + apiKey);
-    }
-
-    private void evaluateAccount(JsonObject acct) {
-        accountType = acct.getString("AccountType");
-        apiKey = acct.getString("ApiKey");
-        codesRequestedToday = acct.getInt("CodesRequestedToday");
-        //this.name = acct.get("Name").asString();
-        //this.company = acct.get("Company").asString();
-    }
-
-    private void evaluateAccount() {
-        accountType = null;
-        apiKey = null;
-        codesRequestedToday = -1;
-    }
-
-    // UNTESTED!!
-    public void login() throws MalformedURLException, IOException, LoginException, URISyntaxException {
-        JsonObjectBuilder builder = Json.createObjectBuilder();
-        builder.add("Email", email);
-        builder.add("Password", password);
-        JsonObject jsonObject = builder.build();
-        String payload = jsonObject.toString();
-
-        JsonObject response = postAndGetObject("/account/login", payload);
-        String status = response.getString("Status");
-        if (!status.equals("success"))
-            throw new LoginException(response.getString("Message"));
-        JsonObject acct = response.getJsonObject("Account");
-        evaluateAccount(acct);
-    }
-
-    public void logout() throws MalformedURLException, IOException, LoginException, URISyntaxException {
-        if (apiKey == null)
-            throw new LoginException("Not logged in");
-        JsonObject response = postAndGetObject("/account/logout" + apiKeyString('?'), "");
-        String status = response.getString("Status");
-        if (!status.equals("success"))
-            throw new LoginException(response.getString("Message"));
-        evaluateAccount();
-    }
-
 
     private JsonArray getJsonArray(String str) throws IOException, URISyntaxException {
         return readFrom(str).asJsonArray();
     }
-
 
     private JsonObject getJsonObject(String str) throws IOException, URISyntaxException {
         return readFrom(str).asJsonObject();
@@ -249,10 +121,9 @@ public class ControlTowerIrDatabase extends DatabaseImporter implements IRemoteS
     private Map<String, String> getMap(String urlFragment, String keyName, String valueName) throws IOException, URISyntaxException {
         JsonArray array = getJsonArray(urlFragment);
         Map<String,String> map = new HashMap<>(array.size());
-        for (JsonValue val : array) {
-            JsonObject obj = val.asJsonObject();
+        array.stream().map(val -> val.asJsonObject()).forEachOrdered(obj -> {
             map.put(obj.getString(keyName), obj.getString(valueName));
-        }
+        });
         return map;
     }
 
@@ -262,14 +133,6 @@ public class ControlTowerIrDatabase extends DatabaseImporter implements IRemoteS
 
     private void loadTypes() throws IOException, URISyntaxException {
         typesMap = getMap("types", "Name", "$id");
-    }
-
-    public String getStatus() throws IOException, URISyntaxException {
-        if (apiKey == null)
-            return "Not logged in";
-        JsonObject obj = getJsonObject("account" + apiKeyString('?'));
-        evaluateAccount(obj);
-        return obj.toString();
     }
 
     public Collection<String> getManufacturers() throws IOException, URISyntaxException {
@@ -313,28 +176,6 @@ public class ControlTowerIrDatabase extends DatabaseImporter implements IRemoteS
                 "Function", "$id").keySet();
     }
 
-    @SuppressWarnings("UseOfSystemOutOrSystemErr")
-    public void getCodeset(int setId, boolean email) throws IOException, LoginException, URISyntaxException {
-        if (apiKey == null)
-            throw new LoginException("Must be logged in");
-        if (email) {
-            JsonObject obj = getJsonObject("codesets/" + Integer.toString(setId) + "?output=email" + apiKeyString('&'));
-            if (!obj.getString("Status").equals("success")) {
-                System.err.println(obj.getString("Message"));
-            }
-            String str = obj.toString();
-            System.out.println(str);
-        } else {
-            JsonObject obj = getJsonObject("codesets/" + Integer.toString(setId) + "?output=direct" + apiKeyString('&'));
-            System.out.println(obj.toString());
-            if (!obj.getString("Status").equals("success")) {
-                System.err.println(obj.getString("Message"));
-            }
-            String str = obj.toString();
-            System.out.println(str);
-        }
-    }
-
     public void load(String manufacturerKey, String deviceTypeKey, String codeSet) throws IOException, URISyntaxException {
         clearCommands();
         manufacturer = manufacturerKey;
@@ -367,12 +208,10 @@ public class ControlTowerIrDatabase extends DatabaseImporter implements IRemoteS
         remoteSet = new RemoteSet(getCreatingUser(), origin, remote);
     }
 
-
     @Override
     public RemoteSet getRemoteSet() {
         return remoteSet;
     }
-
 
     @Override
     public String getFormatName() {
